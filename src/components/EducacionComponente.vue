95d2417d98748aa536e1755dbdbe955704b8831c
Gustavo <150718843+gus-m-rodriguez@users.noreply.github.com>
2024-11-09 15:27:17 -0300

    Update EducacionComponente.vue

--- src/components/EducacionComponente.vue
+++ src/components/EducacionComponente.vue
@@ -11,7 +11,7 @@ fechacolor.value= [
 ];
 const educacion = ref ([]);
 educacion.value = [
-    {fecha: "2024", title: "Técnicatura Universitaria en Programacion", descripcion: "Incumbencias Profecionales: Operacion y programacion de computadoras, desarrollo de programa en distintos lenguajes, analisis y control de sistema informaticos", enlace:"https://www.youtube.com/"},
+    {fecha: "2024", title: "Técnicatura Universitaria en Programacion" descripcion: "Incumbencias Profecionales: Operacion y programacion de computadoras, desarrollo de programa en distintos lenguajes, analisis y control de sistema informaticos", enlace:"https://www.youtube.com/"},
     {fecha: "2023", title: "Ingenieria en Sistemas UdeMM" descripcion:"Actualmente me encuentro cursando el segundo año de la carrera de referencia, ampliando conocimientos en las bases de los sistemas y el desarrollo de aplicaciones, ganando aptitudes en el manerjo de herramietas como UML", enlace: "https://udemm.edu.ar"},
     {fecha: "2022", title: "Intership en Desarrollo web", descripcion: "Realice una pasantia en ABC solutions, contribuyendo en la creacion de interfaces de usuarios y optimizaciión de sitios web", enlace: "http:www.direccion.com"},
     {fecha: "2021", title: "Proyecto personal - Aplicacion de Gestion de tareas", descripcion: "Desarrolle una aplicacion apra la gestión de tareas diarias usando HTML, CSS y JavaScript, implementando funcionalidades como lista de tareas y recordatorios", enlace: "http:www.direccion.com"},
